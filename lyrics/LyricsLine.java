d3a570e1d50a96f3ebd4f799afadced52c1607dc
harnenim <harne_@naver.com>
2014-11-23 01:26:04 +0900

    Create LyricsLine.java

---
+++ lyrics/LyricsLine.java
@@ -0,0 +1,30 @@
+package lyrics;
+
+public class LyricsLine {
+	private String  won;
+	private String  dok;
+	private String  hae;
+	private int     type;
+	
+	public LyricsLine() {
+		this("","","",0);
+	}
+	public LyricsLine(String won, String dok, String hae,int type) {
+		this.won  = won;
+		this.dok  = dok;
+		this.hae  = hae;
+		this.type = type;
+	}
+	public String getWon() {
+		return won;
+	}
+	public String getDok() {
+		return dok;
+	}
+	public String getHae() {
+		return hae;
+	}
+	public int getType() {
+		return type;
+	}	
+}
